9e4acffaa3b4059a552352938695e2a27af4fb85
admin <mounkaila144@gmail.com>
2025-02-15 18:57:59 +0100

    Revert "add login"
    This reverts commit 0cf7f268827f1989935bdb5b20c9ea56bb8bf82c.

--- frontend/app/dashboard/articles/page.tsx
+++ frontend/app/dashboard/articles/page.tsx
@@ -47,7 +47,7 @@ export default function ArticlesPage() {
 
     const queryFilters = {
         ...(search && { search }),
-        ...(categoryId !== 'all' && { categoryId: Number(categoryId) }),
+        ...(categoryId !== 'all' && { categoryId }),
         ...(minPrice !== undefined && { minPrice }),
         ...(maxPrice !== undefined && { maxPrice }),
         ...(sortBy && { sortBy }),
@@ -55,7 +55,6 @@ export default function ArticlesPage() {
         page: currentPage,
         limit: pageSize
     };
-
 
     // Hooks d'API
     const { data, isLoading: isLoadingArticles } = useGetArticlesQuery(queryFilters);
