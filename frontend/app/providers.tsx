e6018f48326b5db45554e18774b87f0fa0f7cb34
admin <mounkaila144@gmail.com>
2025-02-15 23:46:36 +0100

    add login

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,6 +1,5 @@
-// app/providers.tsx
 "use client";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '@/lib/redux/store';
@@ -8,24 +7,33 @@ import { ThemeProvider } from "@/components/theme-provider";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import { usePathname, useSearchParams } from 'next/navigation';
 
+function QueryListener({ onQueryChange }: { onQueryChange: () => void }) {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        onQueryChange();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [pathname, searchParams]);
+
+    return null;
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
     const [isClient, setIsClient] = useState(false);
     const [isPageLoading, setIsPageLoading] = useState(false);
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
 
     useEffect(() => {
         setIsClient(true);
     }, []);
 
-    useEffect(() => {
+    const handleQueryChange = () => {
         setIsPageLoading(true);
         const timeout = setTimeout(() => {
             setIsPageLoading(false);
-        }, 500); // Minimum display time for the spinner
-
+        }, 500); // Temps minimal d'affichage du spinner
         return () => clearTimeout(timeout);
-    }, [pathname, searchParams]);
+    };
 
     if (!isClient) {
         return <LoadingSpinner />;
@@ -41,7 +49,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
                     storageKey="theme"
                     disableTransitionOnChange
                 >
-                    {isPageLoading ? <LoadingSpinner /> : children}
+                    <Suspense fallback={<LoadingSpinner />}>
+                        <QueryListener onQueryChange={handleQueryChange} />
+                        {isPageLoading ? <LoadingSpinner /> : children}
+                    </Suspense>
                 </ThemeProvider>
             </PersistGate>
         </Provider>
